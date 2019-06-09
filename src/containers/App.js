ea0e3c33f8fde848909034e4785269d1fe9c65ae
Huan Ming Liao <hmliao14@gmail.com>
2019-06-08 23:07:01 -0700

    Removed initial files. Structured app.js with semantic ele

---
+++ src/containers/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Netflix
+      </header>
+      <main className="App-main">
+        Main
+      </main>
+      <footer className="App-footer">
+        Footer
+      </footer>
+    </div>
+  );
+}
+
+export default App;
