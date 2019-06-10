66c5475602df6ce06954cd5058cc8719b8c2ef41
Huan Ming Liao <hmliao14@gmail.com>
2019-06-10 01:46:49 -0700

    Updates

---
+++ precache-manifest.b12a91dbb7f22ea16bdea4c186431162.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fb78fdde8972f86baeb45d29a76f66",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "a7d71c8a0b7f147e5eac",
+    "url": "/netflix-clone/static/css/main.36a454c0.chunk.css"
+  },
+  {
+    "revision": "2d28df3dcec46c827c45",
+    "url": "/netflix-clone/static/js/2.989d0642.chunk.js"
+  },
+  {
+    "revision": "a7d71c8a0b7f147e5eac",
+    "url": "/netflix-clone/static/js/main.a8c6b9c4.chunk.js"
+  },
+  {
+    "revision": "ded34aa85da1aff6ad44",
+    "url": "/netflix-clone/static/js/runtime~main.21c42e60.js"
+  },
+  {
+    "revision": "c72f85812bcbd8dbd008aaeeb47df057",
+    "url": "/netflix-clone/static/media/style.c72f8581.styl"
+  },
+  {
+    "revision": "fbc4981ed6d732fdeb63854f0b0e8d3b",
+    "url": "/netflix-clone/static/media/style.fbc4981e.styl"
+  }
+]);
